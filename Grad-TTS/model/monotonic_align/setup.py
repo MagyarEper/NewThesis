adf12f9eea61709af9fcf1abf5991ecda90e8e58
MagyarEper <makai.sandor94@gmail.com>
2026-01-23 20:46:22 +0100

    Fix monotonic_align setup.py output directory issue

--- Grad-TTS/model/monotonic_align/setup.py
+++ Grad-TTS/model/monotonic_align/setup.py
@@ -2,10 +2,20 @@
 
 from distutils.core import setup
 from Cython.Build import cythonize
+from distutils.extension import Extension
 import numpy
 
+# Fix output directory to current directory
+extensions = [
+    Extension(
+        "core",
+        ["core.pyx"],
+        include_dirs=[numpy.get_include()]
+    )
+]
+
 setup(
-    name = 'monotonic_align',
-    ext_modules = cythonize("core.pyx"),
-    include_dirs=[numpy.get_include()]
+    name='monotonic_align',
+    ext_modules=cythonize(extensions),
 )
+
